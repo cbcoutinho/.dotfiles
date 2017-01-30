0174d8bc9c12e815302dbd5478c7d43f072df26e
cbcoutinho <chrisbcoutinho@gmail.com>
2017-01-30 13:04:35 +0100

    Reformat loop to include .bashrc and .profile

--- deploy.sh
+++ deploy.sh
@@ -3,31 +3,33 @@
 
 set -e
 
-# If .atom directory exists...
-if [ -d "$HOME/.atom" ]; then
+files="$(find -type f | grep -v '.git' | grep -v 'README.md' | grep -v 'deploy.sh')"
+# echo $files
 
-  for file in $(ls .atom/)
-  do
-    localfile="$(pwd)/.atom/$file"
-    target="$HOME/.atom/$file"
-    # echo $localfile
-    # echo $target
+for file in $files
+do
+  localfile="$(pwd)/$file"
+  target="$HOME/$file"
+  # echo $localfile
+  # echo $target
 
-    # If target file already exists, delete or move it...
-    if [ -f "$target" ]; then
+  # If target file already exists, delete or move it...
+  if [ -f "$target" ]; then
 
-      # If file is a symlink, delete it, otherwise move it
-      if [ -L "$target" ]; then
-        echo "Is a link! Destroy"
-        rm "$target"
-      else
-        echo "Is not a link! Move!"
-        mv "$target" "$target.orig"
-      fi
-
+    # If file is a symlink, delete it, otherwise move it
+    if [ -L "$target" ]; then
+      echo "Is a link! Destroy"
+      rm "$target"
+    else
+      echo "Is not a link! Move!"
+      mv "$target" "$target.orig"
     fi
 
-    ln -s $localfile $target
+  fi
+  # echo $localfile
+  # echo $target
 
-  done
-fi
+  ln -s $localfile $target
+
+done
+# fi
