1da1e017719fa94a9edba8b475a23e3172e19cdf
cbcoutinho <chrisbcoutinho@gmail.com>
2018-02-19 22:29:19 +0100

    Use `set -x` in pull-git-repos.sh

--- scripts/pull-git-repos.sh
+++ scripts/pull-git-repos.sh
@@ -3,6 +3,9 @@
 # A simple script to pull all software from git repositories in the
 # $HOME/Software directory
 
+# Show lines being run
+set -x
+
 for ii in `find $HOME/Software -type d -name '.git' -prune -exec dirname {} \;`; do
 echo && echo $ii
 git -C $ii pull --recurse-submodules
