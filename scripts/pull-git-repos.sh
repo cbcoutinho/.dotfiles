a872762a4092015fca8b7fc34d8a701e15ee00c6
Chris Coutinho <chrisbcoutinho@gmail.com>
2018-03-26 12:16:01 +0200

    Add fetch to pull-repos script

--- scripts/pull-git-repos.sh
+++ scripts/pull-git-repos.sh
@@ -8,6 +8,7 @@ set -x
 
 for ii in `find $HOME/Software -type d -execdir test -d {}/'.git' \; -prune -print`; do
 echo && echo $ii
+git -C $ii fetch --all --tags
 git -C $ii submodule update --init --recursive
 git -C $ii pull --recurse-submodules
 done
