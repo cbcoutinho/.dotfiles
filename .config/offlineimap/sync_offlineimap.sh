6187a4555b7467ba2ef581cc189ef0965657f042
Chris Coutinho <chrisbcoutinho@gmail.com>
2018-04-06 22:29:14 +0200

    Add some comments about gpg

--- .config/offlineimap/sync_offlineimap.sh
+++ .config/offlineimap/sync_offlineimap.sh
@@ -25,6 +25,14 @@
 # The GPG_AGENT_INFO info isn't exported by gpg-agent above
 #export GPG_AGENT_INFO
 
+# Also an option to source the gpg-agent variables, use the following
+# in crontab:
+#	eval $(cat /home/chris/.gnupg/gpg-agent.env) && export GPG_AGENT_INFO && /home/chris/.config/offlineimap/sync_offlineimap.sh
+#
+# NOTE: This assumes that gpg-agent was called using the
+# `write-env-file` option, which is deprecated in gpg 2.1
+
+
 # Throw away stdout, send stderr to shell/cron
 if [ $(hostname) = 'tumbleweed' ]; then
 	offlineimap -a ChrisCoutinho -u syslog >/dev/null & pid1=$!
--- .zshenv
+++ .zshenv
@@ -6,3 +6,7 @@
 # Tmux has trouble accessing the gpg-agent otherwise
 #	https://unix.stackexchange.com/a/334326/171562
 export GPG_TTY=${TTY}
+
+# Refresh gpg-agent tty in case user switches to an X session
+#	https://wiki.archlinux.org/index.php/GnuPG#Configure_pinentry_to_use_the_correct_TTY
+#gpg-connect-agent updatestartuptty reloadagent /bye >/dev/null 2>&1
