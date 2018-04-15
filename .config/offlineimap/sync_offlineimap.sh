e6e4c8d0ae026a7f6a93562fbcbcea4ad976a2f1
Chris Coutinho <chrisbcoutinho@gmail.com>
2018-04-15 14:19:29 +0200

    Update sync_offlineimap.sh

--- .config/offlineimap/sync_offlineimap.sh
+++ .config/offlineimap/sync_offlineimap.sh
@@ -1,36 +1,8 @@
 #!/usr/bin/env sh
 
 # This script relies on the fact that a gpg-agent is running and has
-# output its env-file in ~/.gnupg/gpg-agent.env
+# output its env file in ~/.gnupg/gpg-agent.env
 # /etc/profile.d/gpg-agent.sh
-
-# # This file sets up a proper GPG daemon on login for all users except
-# # the root user. Originally found:
-# #	https://unix.stackexchange.com/q/131772/171562
-#
-# if [ $EUID -ne 0 ] ; then
-#     envfile="$HOME/.gnupg/gpg-agent.env"
-#     if [[ -e "$envfile" ]] && kill -0 $(grep GPG_AGENT_INFO "$envfile" | cut -d: -f 2) 2>/dev/null; then
-#         eval "$(cat "$envfile")"
-#     else
-#         eval "$(gpg-agent --daemon --enable-ssh-support --write-env-file "$envfile")"
-#     fi
-#     export GPG_AGENT_INFO  # the env file does not contain the export statement
-#     export SSH_AUTH_SOCK   # enable gpg-agent for ssh
-# fi
-
-
-#source $HOME/.gnupg/gpg-agent.env
-
-# The GPG_AGENT_INFO info isn't exported by gpg-agent above
-#export GPG_AGENT_INFO
-
-# Also an option to source the gpg-agent variables, use the following
-# in crontab:
-#	eval $(cat /home/chris/.gnupg/gpg-agent.env) && export GPG_AGENT_INFO && /home/chris/.config/offlineimap/sync_offlineimap.sh
-#
-# NOTE: This assumes that gpg-agent was called using the
-# `write-env-file` option, which is deprecated in gpg 2.1
 
 
 # Throw away stdout, send stderr to shell/cron
@@ -40,34 +12,36 @@ if [ $(hostname) = 'tumbleweed' ]; then
 
 	wait $pid1
 	wait $pid2
-	#echo "OfflineIMAP - Last execution: $(date)"
 else
 	offlineimap -a Work -u syslog >/dev/null
 fi
 
-# Notify user
-#	Using `while read` instead of `for`:
+# Using `while read` instead of `for`:
 #	https://stackoverflow.com/a/9612560/5536001
-#	Using awk to total output:
+# Using awk to total output:
 #	https://stackoverflow.com/a/13728129/5536001
-
-# Correctly turn off spaces and globbing in shell
+# Correctly turn off spaces and globing in shell
 #	https://unix.stackexchange.com/a/9500/171562
-function myfun {
-	# This function expects to have an input of a mailbox name
+
+function count_new_msgs {
+	# The function counts the number of new messages, which based on
+	# the 'Maildir' directory format are contained in a 'new'
+	# subdirectory.
+	# This function expects to have a mailbox name as input and
+	# returns the number of new messages.
+
+	# IFS needs to be overridden
 	IFS='
 	'
 	set -f
 	for dir in $(find $HOME/.mail/$1 -name 'new')
 	do
 		find $dir -type f | wc -l
-	done
+	done | awk '{total += $1} END {print total}'
 }
 
-# Calculate number of new messages using crazy one-liner
-#new=$(find $HOME/.mail -name 'new' -print0 | while read -d $'\0' dir; do; find $dir -type f | wc -l; done | awk '{total += $1} END {print total}')
-gmail=$(myfun 'gmail' | awk '{total += $1} END {print total}')
-office365=$(myfun 'office365' | awk '{total += $1} END {print total}')
+gmail=$(count_new_msgs 'gmail')
+office365=$(count_new_msgs 'office365')
 
 if [ $gmail -gt 0 ] || [ $office365 -gt 0 ]
 then
