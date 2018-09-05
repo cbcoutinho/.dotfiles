1bd3a1b35aa74667babccd32b5e4c4f07cb88390
Chris Coutinho <chrisbcoutinho@gmail.com>
2018-09-05 22:08:09 +0200

    Add email to sent tag

--- .config/offlineimap/postsync.sh
+++ .config/offlineimap/postsync.sh
@@ -5,6 +5,7 @@
 
 notmuch new
 notmuch tag -inbox +sent from:chrisbcoutinho@gmail.com
+notmuch tag -inbox +sent from:cc162@humboldt.edu
 notmuch tag -inbox +sent from:c.coutinho@redstack.nl
 notmuch tag -inbox +sent from:ccoutinho@a-hak.nl
 notmuch tag -inbox +promotions unsubscribe
