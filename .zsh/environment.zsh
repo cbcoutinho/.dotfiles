fb35ea4bd9ac81572e624f2ce1995805eff7e3a3
Chris Coutinho <c.coutinho@redstack.nl>
2018-09-26 23:46:30 +0200

    Update fzf to use FZF_COMMAND for ctrl t also

--- .zsh/environment.zsh
+++ .zsh/environment.zsh
@@ -6,6 +6,7 @@ export DIFFPROG='nvim -d'
 
 if command -v fd >/dev/null; then
 	export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+	export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 fi
 
 # Pass configuration
