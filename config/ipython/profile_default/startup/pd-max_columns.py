2a010bbc74f9fc2ec74741f6ec0e24ac572f0135
Chris Coutinho <chrisbcoutinho@gmail.com>
2019-05-14 23:58:20 +0200

    Make ipython/pd-max_columns ipython2 compatible

--- config/ipython/profile_default/startup/pd-max_columns.py
+++ config/ipython/profile_default/startup/pd-max_columns.py
@@ -7,7 +7,7 @@ try:
     pd.set_option("display.max_columns", None)
     print("\n-- pd.get_option('display.max_columns') = None")
 
-except ModuleNotFoundError:
+except ImportError:
     print("\n-- Pandas not found - the pd.set_option will not be set")
 
 filename = os.path.abspath(__file__)
