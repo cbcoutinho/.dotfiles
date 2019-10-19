7cd34cb4f1cf8ca216487a800371e173dbf29df2
Chris Coutinho <c.coutinho@focuscura.nl>
2019-10-19 20:08:49 +0200

    Move .gitconfig and .gitignore_global to .config/git

--- deploy.sh
+++ deploy.sh
@@ -34,10 +34,6 @@ ln -s $dotfiles/.gnupg/gpg.conf ~/.gnupg/gpg.conf
 ln -s $dotfiles/.gnupg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
 ln -s $dotfiles/.gnupg/sshcontrol ~/.gnupg/sshcontrol
 
-# Git config and ignore files
-ln -s $dotfiles/.gitconfig ~/.gitconfig
-ln -s $dotfiles/.gitignore_global ~/.gitignore_global
-
 # Node/npm/nvm dot files
 ln -s $dotfiles/.nvmrc ~/.nvmrc
 
