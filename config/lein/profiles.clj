f6c80f0688b201909da0abde049361df768ba784
Chris Coutinho <c.coutinho@focuscura.nl>
2019-10-20 22:38:49 +0200

    Set mouse on for tmux globally, reformat lein/profiles.clj

--- config/lein/profiles.clj
+++ config/lein/profiles.clj
@@ -1,13 +1,14 @@
-{:user {:dependencies [[org.clojure/tools.trace "RELEASE"]
-                       [cljfmt "RELEASE"]
-                       [midje "RELEASE"]
-                       [slamhound "RELEASE"]
+{:user {:dependencies [[org.clojure/tools.trace "0.7.10"]
+                       [cljfmt "0.6.4"]
+                       [midje "1.9.9"]
+                       [slamhound "1.5.5"]
                        [com.bhauman/rebel-readline "0.1.4"]]
 
         :plugins [;[venantius/ultra "0.6.0"]
-                  [lein-cljfmt "0.6.4"]
-                  [lein-midje "3.2.1"]
-                  [lein-jupyter "0.1.16"]]
+                  [lein-cljfmt "0.6.4"]     ; Format code on the fly
+                  [lein-midje "3.2.1"]      ; Testing framework plugin
+                  [lein-jupyter "0.1.16"]   ; Use clojupyter from leiningen
+                  [venantius/pyro "0.1.2"]] ; Booster pack for stack-traces
 
         :aliases {"slamhound" ["run" "-m" "slam.hound"]
                   "rebl" ["trampoline" "run" "-m" "rebel-readline.main"]}
